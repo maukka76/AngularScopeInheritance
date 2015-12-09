5e81ca419b07988b0c43fd7d2c6773a92242685c
maukka76 <markus.veijola@gmail.com>
2015-12-09 19:23:22 +0200

    example code added

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var module = angular.module('module',[]);
+
+module.controller('exampleController',function($scope){
+    
+    $scope.message = 'Cruel World';
+    $scope.isAdmin = true;
+});
+
---
+++ solution.js
@@ -0,0 +1,6 @@
+module.controller('exampleController2',function($scope){
+    
+    $scope.data = {};
+    $scope.data.message = 'Cruel World';
+    $scope.data.isAdmin = true;
+});
